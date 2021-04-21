24b3e85f553eb8694e20c621d0d7173b6b55f096
raihanastrada <raihan.astrada@gmail.com>
2021-04-21 19:15:43 +0700

    update ci, bikin exe buat semua file py

--- src/CoronaApp.py
+++ src/CoronaApp.py
@@ -2,16 +2,17 @@ from loginUI import startLogin as runApp
 from initializedatabase import initializeDatabase as initDb
 import sqlite3
 
-conn = sqlite3.connect("sistem-tracking-corona.db")
-c = conn.cursor()
-try:
-    c.execute("SELECT * FROM t_akun")
-    c.execute("SELECT * FROM t_harian")
-    c.execute("SELECT * FROM t_rs")
-    conn.commit()
-    conn.close()
-except:
-    conn.commit()
-    conn.close()
-    initDb()
-runApp()
+if __name__ == "__main__":
+    conn = sqlite3.connect("sistem-tracking-corona.db")
+    c = conn.cursor()
+    try:
+        c.execute("SELECT * FROM t_akun")
+        c.execute("SELECT * FROM t_harian")
+        c.execute("SELECT * FROM t_rs")
+        conn.commit()
+        conn.close()
+    except:
+        conn.commit()
+        conn.close()
+        initDb()
+    runApp()
